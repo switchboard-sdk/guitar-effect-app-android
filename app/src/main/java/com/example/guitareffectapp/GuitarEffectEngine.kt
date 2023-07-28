ad442d713650b191d86b5d2c87cca398ee18e79e
Balazs Kiss <dev@balazskiss.name>
2023-07-29 01:49:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/guitareffectapp/GuitarEffectEngine.kt
@@ -0,0 +1,52 @@
+package com.example.guitareffectapp
+
+import android.content.Context
+import com.synervoz.switchboard.sdk.audioengine.AudioEngine
+import com.synervoz.switchboard.sdk.audiograph.AudioGraph
+import com.synervoz.switchboard.sdk.audiographnodes.GainNode
+import com.synervoz.switchboardsuperpowered.audiographnodes.CompressorNode
+import com.synervoz.switchboardsuperpowered.audiographnodes.FlangerNode
+import com.synervoz.switchboardsuperpowered.audiographnodes.GuitarDistortionNode
+import com.synervoz.switchboardsuperpowered.audiographnodes.ReverbNode
+
+class GuitarEffectEngine(context: Context) {
+    val audioEngine = AudioEngine()
+    val audioGraph = AudioGraph()
+    val gainNode = GainNode()
+
+    val compressorNode = CompressorNode()
+    val flangerNode = FlangerNode()
+    val guitarDistortionNode = GuitarDistortionNode()
+    val reverbNode = ReverbNode()
+
+    init {
+        audioGraph.addNode(gainNode)
+
+        audioGraph.addNode(compressorNode)
+        audioGraph.addNode(flangerNode)
+        audioGraph.addNode(guitarDistortionNode)
+        audioGraph.addNode(reverbNode)
+
+        audioGraph.connect(audioGraph.inputNode, compressorNode)
+        audioGraph.connect(compressorNode, flangerNode)
+        audioGraph.connect(flangerNode, guitarDistortionNode)
+        audioGraph.connect(guitarDistortionNode, reverbNode)
+        audioGraph.connect(reverbNode, gainNode)
+        audioGraph.connect(gainNode, audioGraph.outputNode)
+        audioEngine.enableMicrophone(true)
+    }
+
+    fun start() {
+        audioEngine.start(audioGraph)
+    }
+
+    fun stop() {
+        audioEngine.stop()
+    }
+
+    fun close() {
+        audioEngine.close()
+        audioGraph.close()
+        gainNode.close()
+    }
+}
---
+++ app/src/main/java/com/example/guitareffectapp/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.guitareffectapp
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.synervoz.switchboard.sdk.SwitchboardSDK
+import com.synervoz.switchboard.ui.UIGenerator
+import com.synervoz.switchboardsuperpowered.SuperpoweredExtension
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var engine: GuitarEffectEngine
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (checkMicrophonePermission()) {
+            initAudioEngine()
+        }
+    }
+
+    fun checkMicrophonePermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission is not granted, ask the user.
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 0)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            0 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    initAudioEngine()
+                }
+            }
+        }
+    }
+
+    private fun initAudioEngine() {
+        SwitchboardSDK.initialize("Your client ID", "Your client secret")
+        SuperpoweredExtension.initialize("ExampleLicenseKey-WillExpire-OnNextUpdate")
+        engine = GuitarEffectEngine(this)
+        engine.start()
+        setContentView(UIGenerator.generateViews(this, engine.audioGraph).getView())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        engine.stop()
+        engine.close()
+    }
+}
